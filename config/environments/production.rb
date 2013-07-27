53e4f2d54f1350a61147f9034f9eb5838f3819cc
willchapin <wrchapin@gmail.com>
2013-07-27 16:49:57 -0400

    add user signup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,9 @@ Tongo::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  
+  # Force SSL
+  config.force_ssl = true
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
