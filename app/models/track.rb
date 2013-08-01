39b1e09d73cf36827faf9fdb83c2fff2301445f2
willchapin <wrchapin@gmail.com>
2013-08-01 12:57:23 -0400

    add default order to tracks

--- app/models/track.rb
+++ app/models/track.rb
@@ -7,6 +7,7 @@ class Track < ActiveRecord::Base
                     s3_credentials: File.join(Rails.root, 'config', 'aws.yml')
 
   belongs_to :user
+  default_scope order: 'created_at DESC'
   validates_attachment :track_file, presence: true,
     content_type: { content_type: /(video|audio)\/ogg/i }
 
