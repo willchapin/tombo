8810a3fc1d894a3e8ded494cfdce86d29036d77b
willchapin <wrchapin@gmail.com>
2013-08-01 19:41:56 -0400

    allow users to delete their own tracks

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,4 +1,8 @@
 class TracksController < ApplicationController
+
+  before_filter :signed_in_user, only: [:delete]
+  before_filter :authorized, only: [:delete]
+
   def create
     @track = current_user.tracks.build(params[:track])
     if @track.save
@@ -11,5 +15,23 @@ class TracksController < ApplicationController
   end
 
   def destroy
+   @track = Track.find(params[:id]).delete
+   flash[:success] = "#{@track.title} has been deleted!"
+   redirect_to root_path
   end
+
+
+  private
+
+    def authorized
+      @track = Track.find(params[:id])
+      @user = @track.user
+      redirect_to root_path unless current_user?(@user)
+    end
+
+    def signed_in_user
+      redirect_to signin_path unless signed_in? 
+    end
+
+
 end
